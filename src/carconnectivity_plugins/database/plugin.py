b051605f6647b2b106517a408c230605ccb1a422
Till Steinbach <steinbach.till@gmx.de>
2025-12-30 12:33:45 +0100

    enabling auto commit  at engine level

--- src/carconnectivity_plugins/database/plugin.py
+++ src/carconnectivity_plugins/database/plugin.py
@@ -54,8 +54,8 @@ class Plugin(BasePlugin):  # pylint: disable=too-many-instance-attributes
         connect_args = {}
         if 'postgresql' in self.active_config['db_url']:
             connect_args['options'] = '-c timezone=utc'
-        self.engine: Engine = create_engine(self.active_config['db_url'], pool_pre_ping=True, connect_args=connect_args)
-        session_factory: sessionmaker[Session] = sessionmaker(bind=self.engine, autoflush=True, autocommit=True)
+        self.engine: Engine = create_engine(self.active_config['db_url'], pool_pre_ping=True, connect_args=connect_args, isolation_level="AUTOCOMMIT")
+        session_factory: sessionmaker[Session] = sessionmaker(bind=self.engine, autoflush=True)
         scoped_session_factory: scoped_session[Session] = scoped_session(session_factory)
         self.session: Session = scoped_session_factory()
 
